d352946d686587d7371216142b91392abce26676
jackyscript <8930712+jackyscript@users.noreply.github.com>
2022-11-14 17:13:18 +0100

    Add monad example

--- js/monad.js
+++ js/monad.js
@@ -1,22 +1,68 @@
-// @Credits to amazing explanation by Martin Novak @MeetMartin
-// JavaScript functional programming
-// Monad under 60 seconds
+/*
+  @Credits to Martin Novak @MeetMartin.
+  JavaScript functional programming.
+  Monad under 60 seconds
+*/
 
+/*
+  This is the simplest implementation of a functor
+  It simply has a "of"-function that accepts a value
+  and returns an object that has "map".
+*/
 const Functor = {
-    of: value => ({
-        value: value,
-        inspect: () => console.log(`Functor(${value})`),
-        map: fn => Functor.of(fn(value))
-    })
-};s
-
-const Monad = {
-    of: value => ({
-        value: value,
-        inspect: () => console.log(`Monad(${value})`),
-        map: fn => Monad.of(fn(value)),
-        flatMap: fn => fn(value)
-    })
+  of: value => ({
+    value: value,
+    inspect: () => console.log(`Functor(${value})`),
+    map: fn => Functor.of(fn(value))
+  })
 };
 
+// To turn our functor into a monad we just add the flatMap function.
+const Monad = {
+  of: value => ({
+    value: value,
+    inspect: () => console.log(`Monad(${value})`),
+    map: fn => Monad.of(fn(value)),
+    flatMap: fn => fn(value)
+  })
+};
+
+
+/*
+  Every monad is a functor. A functor is just a wrapper about some value:
+*/
+Functor.of('string');
+Functor.of(['array']);
+Functor.of(5);
+
+Functor
+  .of(5)
+  .map(a => a + 1);
+// => Functor(6)
+
+/*
+  To be a functor, a functor has to implement "map".
+  "map" applies a function to the value inside of the functor.
+*/
+
+/*
+  If a functor maps a function that returns a monad
+  you would have a monad inside of a monad.
+*/ 
+Monad
+  .of('Turtle')
+  .map(a => Monad.of(a + 's'));
+// => Monad(Monad('Turtles'))
+
+
+/*
+  Instead a monad needs to implement "flatMap".
+  "flatMap" flattens the result so instead of a monad inside of a monad you just have one monad.
+*/
+Monad
+  .of('Turtle')
+  .flatMap(a => Monad.of(a + 's'));
+// => Monad('Turtles')
+
+// Let's apply our monad...
 Monad.of(5).flatMap(a => Monad.of(a + 1)).inspect();
