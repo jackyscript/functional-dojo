5e309855bad491812a05f73bc6f07493eef37a36
jackyscript <jackyscript@users.noreply.github.com>
2024-02-06 23:43:04 +0100

    Add test argument

--- java/demo/src/test/java/com/example/AppTest.java
+++ java/demo/src/test/java/com/example/AppTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
@@ -27,6 +28,9 @@ class AppTest {
         return Stream.of(
                 Arguments.of(6, 5, (Function<Integer, Integer>) (number -> number + 1)),
                 Arguments.of(4, "Four", (Function<String, Integer>) (String::length)),
+                Arguments.of(63, List.of(1, 2, 4, 8, 16, 32),
+                        (Function<List<Integer>, Integer>) (numbers -> numbers.stream().reduce(0,
+                                (a, b) -> Integer.sum(a, b)))),
                 Arguments.of("Turtles", "Turtle", (Function<String, String>) (name -> name + "s")));
     }
 
