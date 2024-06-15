13b43df332d893535db0b876af3fba7e95fe05bb
jackyscript <jackyscript@users.noreply.github.com>
2024-06-16 01:19:13 +0200

    Add more monad tests

--- scala/scala3/src/test/scala/MonadTest.scala
+++ scala/scala3/src/test/scala/MonadTest.scala
@@ -6,7 +6,22 @@ class MonadTest extends munit.FunSuite {
       .of("Turtle")
       .flatMap(_ + "s");
 
+    val sixMonad = Monad
+      .of(5)
+      .flatMap(_ + 1);
+
+    val fourMonad = Monad
+      .of("Four")
+      .flatMap(_.length);
+
+    val sumMonad = Monad
+      .of(List(1, 2, 4, 8, 16, 32))
+      .flatMap(_.sum);
+
     assertEquals(turtlesMonadFlat, "Turtles");
+    assertEquals(sixMonad, 6);
+    assertEquals(fourMonad, 4);
+    assertEquals(sumMonad, 63);
 
   }
 
